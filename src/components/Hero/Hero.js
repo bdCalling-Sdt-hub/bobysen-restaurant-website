01427be1fe350b436b912cd1efcad14f26b0c31e
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2024-06-11 16:35:52 +0600

    hero animation updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -26,9 +26,11 @@ const pathVariants = {
 const leftDivVariants = {
   initial: {
     opacity: 0,
+    y: -20,
   },
   animate: {
     opacity: 1,
+    y: 0,
     transition: {
       duration: 0.7,
       ease: "easeInOut",
